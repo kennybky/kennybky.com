977f4244b764e87fb0d23fa87197746ac95c7495
kennybky <kennybky92@gmail.com>
2016-12-31 17:27:17 -0800

    Add isaac

---
+++ Transoft/Transoft/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Transoft.MvcApplication" Language="C#" %>
---
+++ Transoft/Transoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Transoft.Startup))]
+namespace Transoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
