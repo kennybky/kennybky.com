41ee915b5029be04e95cc6f1fa6a8407a3d65cf0
kennybky <kennybky92@gmail.com>
2017-05-10 12:13:34 -0700

    nyt1

---
+++ NewsCorp/scripts/nyt.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+$(document).ready(function () {
+    $.ajax({
+        url: "http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
+        dataType: "xml",
+        success: function (data) {
+            var items = $(data).find("item").each(function () {
+                var main = $('#main');
+                var article = $('<article></<article>');
+                article.append("<header>" + $(this).find("title").text() + "</header>");
+
+
+
+
+            });
+
+        }
+
+    });
+
+
+});
