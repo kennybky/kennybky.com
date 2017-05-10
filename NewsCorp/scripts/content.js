4974eef42b6c74fdb2f713c5b335667bfc0e8064
kennybky <kennybky92@gmail.com>
2017-05-10 11:25:43 -0700

    Edit

--- NewsCorp/scripts/content.js
+++ NewsCorp/scripts/content.js
@@ -23,4 +23,15 @@ $(document).ready(function () {
 
         }
     });
+
+    $("#menubar").click(function () {
+        $("#menu").toggle(500);
+        var arr = $(this).find("span");
+        var show = arr[0];
+        var hide = arr[1];
+        $(show).toggle();
+        $(hide).toggle();
+
+
+    });
 })
